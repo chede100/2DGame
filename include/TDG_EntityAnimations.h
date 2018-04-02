e654b096474d0845f8a539523a78215692c7b289
chede100 <chris.eder96@googlemail.com>
2018-04-02 20:07:13 +0200

    Update entity animation.

--- include/TDG_EntityAnimations.h
+++ include/TDG_EntityAnimations.h
@@ -2,6 +2,13 @@
 #define TDG_ENTITYANIMATIONS_H
 
 #include "TDG_AnimationList.h"
+#include "TDG_GUI.h"
+#include "TDG_SpriteLoader.h"
+
+typedef enum
+{
+    Player, NPC, Object
+}EntityTyp;
 
 class TDG_EntityAnimations
 {
@@ -9,13 +16,17 @@ class TDG_EntityAnimations
         TDG_EntityAnimations();
         virtual ~TDG_EntityAnimations();
 
-        void loadAnimations(int entityID);
+        bool loadAnimations(TDG_GUI* gui, int entityID, EntityTyp typ);
 
         TDG_Animation* getAnimation(AnimationTyp typ);
 
     protected:
 
     private:
+        void add(TDG_Animation* ani);
+
+        int entityID;
+        EntityTyp typ;
 
         TDG_AnimationList* aniL;
 };
--- src/TDG_EntityAnimations.cpp
+++ src/TDG_EntityAnimations.cpp
@@ -2,10 +2,94 @@
 
 TDG_EntityAnimations::TDG_EntityAnimations()
 {
-    //ctor
+    this->aniL = NULL;
+    this->entityID = 0;
 }
 
 TDG_EntityAnimations::~TDG_EntityAnimations()
 {
-    //dtor
+    TDG_AnimationList* tmp = this->aniL;
+    TDG_AnimationList* next;
+
+    while(tmp != NULL)
+    {
+        next = tmp->getNext();
+        delete tmp;
+        tmp = next;
+    }
+}
+
+bool TDG_EntityAnimations::loadAnimations(TDG_GUI* gui, int entityID, EntityTyp typ)
+{
+    this->entityID = entityID;
+    this->typ = typ;
+
+    string folderPath;
+    if(typ == Player || typ == NPC)
+        folderPath = "./data/img/entity/char/";
+    else if(typ == Object)
+        folderPath = "./data/img/entity/object/";
+
+    string sName = "";
+    ostringstream ss;
+    ss << entityID;
+    sName += ss.str();
+
+    TDG_SpriteLoader* sprite = new TDG_SpriteLoader();
+    if(!sprite->loadSprite(folderPath, sName))
+    {
+        cout << "Unable to load sprite: " << folderPath << sName << endl;
+        return false;
+    }
+
+    add(sprite->getAnimation(gui, 1, move_east));
+    add(sprite->getAnimation(gui, 2, move_north));
+    add(sprite->getAnimation(gui, 3, move_south));
+    add(sprite->getAnimation(gui, 4, stand_east));
+    add(sprite->getAnimation(gui, 5, stand_north));
+    add(sprite->getAnimation(gui, 6, stand_south));
+
+    delete sprite;
+
+    return true;
+}
+
+TDG_Animation* TDG_EntityAnimations::getAnimation(AnimationTyp typ)
+{
+    TDG_AnimationList* tmp = this->aniL;
+
+    while(tmp != NULL)
+    {
+        if(tmp->getAnimation()->getTyp() == typ)
+            return tmp->getAnimation();
+
+        tmp = tmp->getNext();
+    }
+
+    return NULL;
+}
+
+void TDG_EntityAnimations::add(TDG_Animation* ani)
+{
+    if(this->aniL == NULL)
+    {
+        this->aniL = new TDG_AnimationList();
+        aniL->setAnimation(ani);
+    }
+    else
+    {
+        TDG_AnimationList* tmp = this->aniL;
+        TDG_AnimationList* next = this->aniL->getNext();
+
+        while(next != NULL)
+        {
+            tmp = next;
+            next = tmp->getNext();
+        }
+
+        TDG_AnimationList* newAni = new TDG_AnimationList();
+        newAni->setAnimation(ani);
+
+        tmp->setNext(newAni);
+    }
 }
